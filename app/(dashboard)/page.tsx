881984d3afccc15bd6d468c2bca3b529952df1ae
ad3kola <129291078+ad3kola@users.noreply.github.com>
2025-07-03 23:05:09 -0300

    new commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      Dont forget to like, comment and subscribe and clickm the bell so you;re notified when I uolaod on my channel
+      The Chunkz Show
     </div>
   )
 }
