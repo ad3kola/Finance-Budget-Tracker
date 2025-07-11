335a54657f7a43793d1ee30cd5e38f0df2161ea4
ad3kola <129291078+ad3kola@users.noreply.github.com>
2025-07-10 22:18:12 -0300

    new commit

--- lib/supabase-helper.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabase-helper.ts
-import { getToken } from "@clerk/nextjs/server";
-import { createSupabaseClient } from "@/config/supabase-client";
-
-export const getSupabaseClient = async () => {
-  const token = await getToken({ template: "supabase" });
-  return createSupabaseClient(token);
-};
